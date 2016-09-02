68f772da0a09c2e13894940496a60e4924d54746
daniel svanber <daniel.nikten@gmail.com>
2016-09-02 15:01:56 +0200

    added input control and beeps

--- BattleShip Console GIT/BattleShip Console GIT/Program.cs
+++ BattleShip Console GIT/BattleShip Console GIT/Program.cs
@@ -9,7 +9,17 @@ namespace battleship_console
     class Program
     {
         //början på daniels kod
-        static bool[,] GameMap = new bool[7, 5]; //Game field
+        static int[,] GameMap = new int[7, 5]; //Game field
+        //DOCUMENTING
+        /* 0 - nothing (never shot there) - displays as 0
+         * 1 - living ship - never displayed
+         * 2 - dead ship - displayed as X
+         * -1 - miss - displayed as .
+         */
+
+        static bool no_valid_input = true;
+        static int inx = 0;
+        static int iny = 0;
 
         static void Main(string[] args)
         {
@@ -27,12 +37,12 @@ namespace battleship_console
             {
                 for (int x = 0; x < 7; x++)
                 {
-                    GameMap[x, y] = false;
+                    GameMap[x, y] = 0;
                 }
             }
 
             //DEBUG
-            Console.WriteLine("Debug: " + GameMap.GetLength(1)); 
+
 
 
             //create ships
@@ -40,11 +50,11 @@ namespace battleship_console
             {
                 int Xpos = r.Next(0, 6);
                 int Ypos = r.Next(0, 4);
-                if (check_coord(Xpos, Ypos) != true) //Calls Check_coord if a cordinate is empty
+                if (check_ship_exists(Xpos, Ypos) != true) //Calls check_ship_exists if a cordinate is empty
                 {
-                    GameMap[Xpos, Ypos] = true;
+                    GameMap[Xpos, Ypos] = 1; //set ship as living
                 }
-                Console.WriteLine(i.ToString());
+
             }
 
 
@@ -58,27 +68,45 @@ namespace battleship_console
 
             for (int i = 0; i < current_amount_of_bs; i = i)
             {
-                Console.WriteLine("Skepapr kvar: " + current_amount_of_bs);
-                Console.WriteLine("Mata in X koordinat för kanoner att skjuta: ");
-                int inx = int.Parse(Console.ReadLine()) - 1;
-                Console.WriteLine("Mata in Y koordinat för kanoner att skjuta: ");
-                int iny = int.Parse(Console.ReadLine()) - 1;
-
-                if (check_coord(inx, iny))
+                while (no_valid_input)
                 {
-                    GameMap[inx, iny] = false;
-                    Console.Beep(420, 150);
+                    Console.WriteLine("Skepapr kvar: " + current_amount_of_bs);
+                    Console.WriteLine("Mata in X koordinat för kanoner att skjuta: ");
+                    inx = control_input(Console.ReadLine(), 7);
+                    Console.WriteLine("Mata in Y koordinat för kanoner att skjuta: ");
+                    iny = control_input(Console.ReadLine(), 5);
+                    if (inx != -1 && iny != -1)
+                    {
+                        inx--; iny--;
+                        no_valid_input = false; //input is valid, exit loop
+                    }
+                    else
+                    {
+                        Console.WriteLine("***MATA IN ORDENTLIGA KOORDINATER!***");
+                        Console.Beep(500, 500);
+                        Console.Beep(500, 250);
+                    }
+                }
+
+                if (check_ship_exists(inx, iny))
+                {
+                    GameMap[inx, iny] = 2; //sunken ship
+                    Console.Beep(2000, 150);
                     Console.WriteLine("Du träffade!");
                     current_amount_of_bs--;
                     Draw_game_map(inx, iny, true);
                 }
                 else
                 {
+                    GameMap[inx, iny] = -1; //missmark
                     Console.WriteLine("Du missade!");
                     Draw_game_map(inx, iny, false);
                 }
                 shots_fired++;
                 Console.WriteLine("Skott avfyrade: " + shots_fired);
+
+                no_valid_input = true; //loop for loop again
+
 
             }
             Console.WriteLine("Du vann! Tryck på en knapp för att avsluta programmet.");
@@ -88,9 +116,9 @@ namespace battleship_console
         }
 
         //Början på Alekseis metoder
-        static bool check_coord(int x, int y) //
+        static bool check_ship_exists(int x, int y) //
         {
-            if (GameMap[x, y] == true)
+            if (GameMap[x, y] == 1)
             {
                 return true;
             }
@@ -99,6 +127,30 @@ namespace battleship_console
                 return false;
             }
         }
+
+
+
+
+        //converts first symbol of string to char and controls if it's in range of
+        //1 to 9 (all inclusive) by comparing it to ASCII table
+        //also checks if input range is correct
+        //returns -1 if input was incorrect in any way
+        static int control_input(string input, int limit)
+        {
+            int output = -1;
+
+            if (input != string.Empty)
+            {
+                int number = int.Parse(input);
+                if (number > 0 && number < limit)
+                {
+                    output = number;
+                }
+            }
+            return output;
+        }
+
+
         //Slut på Alekseis kod
 
         //Mer kod av daniel
@@ -114,7 +166,7 @@ namespace battleship_console
                     }
                     else
                     {
-                        Console.Write("O");
+                        Console.Write("0");
                     }
                 }
                 Console.WriteLine();
