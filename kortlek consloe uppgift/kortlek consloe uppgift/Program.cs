a46de3f10b94e12b0c2539f2a22c5778da808444
daniel svanber <daniel.nikten@gmail.com>
2016-09-05 15:23:15 +0200

    Base code made. Not funktional.

---
+++ kortlek consloe uppgift/kortlek consloe uppgift/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kortlek_consloe_uppgift
+{
+    class Program
+    {
+        static int[,] card = new int[4, 13]; //create array for cards
+
+        /*
+         * first int will be type. second what card number.
+         * 1 = hjärter
+         * 2 = ruter 
+         * 3 = spader 
+         * 4 = klöver
+         */
+
+
+        static void Main(string[] args)
+        {
+            int Cards_in_deck = 52;
+            string type = string.Empty;
+            string number = string.Empty;
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; i++)
+                {
+                   card[i, j] = 0; //0 means not drawn. 1 means drawn.
+                }
+            }
+
+            Console.WriteLine("Tryck enter för att dra ett kort");
+            Console.ReadLine();
+            Random r = new Random();
+            int type_of_card = r.Next(0, 3) + 1; // deterimin what card type. Add one so it will start at one and go up to 4.
+            int number_of_card = r.Next(0, 12) + 1; // determind the card number. Ace is one. Add one so the starting value is one.
+
+            for (int i = 0; i < Cards_in_deck; i++)
+            {
+                Console.ReadLine();
+                if (card[type_of_card, number_of_card] == 0)
+                {
+                    switch (type_of_card)
+                    {
+                        case 1:
+                            {
+                                type = "hjärter";
+                                break;
+                            }
+                        case 2:
+                            {
+                                type = "ruter";
+                                break;
+                            }
+                        case 3:
+                            {
+                                type = "spader";
+                                break;
+                            }
+                        case 4:
+                            {
+                                type = "klöver";
+                                break;
+                            }
+                    }
+                    switch (number_of_card)
+                    {
+                        case 1:
+                            {
+                                number = "ess";
+                                break;
+                            }
+                        case 2:
+                            {
+                                number = "2";
+                                break;
+                            }
+                        case 3:
+                            {
+                                number = "3";
+                                break;
+                            }
+                        case 4:
+                            {
+                                number = "4";
+                                break;
+                            }
+                        case 5:
+                            {
+                                number = "5";
+                                break;
+                            }
+                        case 6:
+                            {
+                                number = "6";
+                                break;
+                            }
+                        case 7:
+                            {
+                                number = "7";
+                                break;
+                            }
+                        case 8:
+                            {
+                                number = "8";
+                                break;
+                            }
+                        case 9:
+                            {
+                                number = "9";
+                                break;
+                            }
+                        case 10:
+                            {
+                                number = "10";
+                                break;
+                            }
+                        case 11:
+                            {
+                                number = "knäckt";
+                                break;
+                            }
+                        case 12:
+                            {
+                                number = "droting";
+                                break;
+                            }
+                        case 13:
+                            {
+                                number = "kung";
+                                break;
+                            }
+                    }
+                    Console.WriteLine(type + " " + number);
+
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        }
+    }
+}
