217ccdb94c95ec1d1c58ba6c7be1a84665585de5
daniel svanber <daniel.nikten@gmail.com>
2016-09-06 12:46:45 +0200

    Fixed an error where the same card came up twice. Added point system. Cleaned up code.

--- kortlek consloe uppgift/kortlek consloe uppgift/Program.cs
+++ kortlek consloe uppgift/kortlek consloe uppgift/Program.cs
@@ -36,8 +36,9 @@ namespace kortlek_consloe_uppgift
             Random r = new Random();
             int type_of_card; // deterimin what card type.
             int number_of_card; // determind the card number. Ace is one.
+            int Score = 0; // detitminds the score of the player
 
-            for (int i = 0; i < Cards_in_deck; i++)
+            for (int i = 0; i < Cards_in_deck;)
             {
                 type_of_card = r.Next(0, 4); //Add one so it will start at one and go up to 4.
                 number_of_card = r.Next(0, 13); //Add one so it will start at one and go up to 4.
@@ -45,24 +46,25 @@ namespace kortlek_consloe_uppgift
                 {
                     Console.WriteLine("Tryck enter för ett kort");
                     Console.ReadLine();
+                    card[type_of_card, number_of_card] = 1;
                     switch (type_of_card)
                     {
-                        case 1:
+                        case 0:
                             {
                                 type = "hjärter";
                                 break;
                             }
-                        case 2:
+                        case 1:
                             {
                                 type = "ruter";
                                 break;
                             }
-                        case 3:
+                        case 2:
                             {
                                 type = "spader";
                                 break;
                             }
-                        case 4:
+                        case 3:
                             {
                                 type = "klöver";
                                 break;
@@ -70,82 +72,92 @@ namespace kortlek_consloe_uppgift
                     }
                     switch (number_of_card)
                     {
-                        case 1:
+                        case 0:
                             {
                                 number = "ess";
+                                Score = Score + 1;
+                                break;
+                            }
+                        case 1:
+                            {
+                                number = "2";
+                                Score = Score + 2;
                                 break;
                             }
                         case 2:
                             {
-                                number = "2";
+                                number = "3";
+                                Score = Score + 3;
                                 break;
                             }
                         case 3:
                             {
-                                number = "3";
+                                number = "4";
+                                Score = Score + 4;
                                 break;
                             }
                         case 4:
                             {
-                                number = "4";
+                                number = "5";
+                                Score = Score + 5;
                                 break;
                             }
                         case 5:
                             {
-                                number = "5";
+                                number = "6";
+                                Score = Score + 6;
                                 break;
                             }
                         case 6:
                             {
-                                number = "6";
+                                number = "7";
+                                Score = Score + 7;
                                 break;
                             }
                         case 7:
                             {
-                                number = "7";
+                                number = "8";
+                                Score = Score + 8;
                                 break;
                             }
                         case 8:
                             {
-                                number = "8";
+                                number = "9";
+                                Score = Score + 9;
                                 break;
                             }
                         case 9:
                             {
-                                number = "9";
+                                number = "10";
+                                Score = Score + 10;
                                 break;
                             }
                         case 10:
                             {
-                                number = "10";
+                                number = "knäckt";
+                                Score = Score + 11;
                                 break;
                             }
                         case 11:
                             {
-                                number = "knäckt";
+                                number = "droting";
+                                Score = Score + 12;
                                 break;
                             }
                         case 12:
                             {
-                                number = "droting";
-                                break;
-                            }
-                        case 13:
-                            {
                                 number = "kung";
+                                Score = Score + 13;
                                 break;
                             }
                     }
-                    Console.WriteLine("Ditt kort är: " + type + " " + number);
-                    card[type_of_card, number_of_card] = 1;
-
-                }
-                else
-                {
-                    i--;
+                    Console.WriteLine("Ditt kort är: " + type + " " + number + "\n" +" Du har " + (Cards_in_deck - i - 1) + " kort kvar");
+                    Console.WriteLine("Ditt poäng är: " + Score);
+                    i++;
                 }
             }
 
+            Console.WriteLine();
             Console.WriteLine("Du är klar");
             Console.ReadLine();
         }
